2cdd3d54920dd64ecac669ca61df52c2a478135e
2142920 <162396015+2142920@users.noreply.github.com>
2024-06-16 01:40:43 +0900

    Create 3.cpp

---
+++ 3.cpp
@@ -0,0 +1,32 @@
+/*
+#include <iostream>
+using namespace std;
+
+void gugu(int a, int b);
+
+int main() {
+	int a;
+	int b;
+	cout << "구구단할 숫자를 입력하시오: " << endl;
+	cin >> a >> b;
+
+	gugu(a, b);
+	return 0;
+}
+
+void gugu(int a,int b) {
+	if (a <= b) {
+		for (int i = a; i <= b; i++) {
+			cout << i << "단" << endl;
+			for (int j= 1; j<= 9; j++){
+				cout << i<< "*" << j<< "=" << i* j<< endl;
+			}
+		}
+			
+	}
+	else {
+		cout << "b가 a보다 작아야 합니다."<<endl;
+	}
+
+}
+*/
