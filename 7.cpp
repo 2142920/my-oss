20d0c7762c1581612dcf5b37d02bc1de23d902b8
2142920 <162396015+2142920@users.noreply.github.com>
2024-06-16 01:41:37 +0900

    Create 7.cpp

---
+++ 7.cpp
@@ -0,0 +1,36 @@
+/*
+#include <iostream>
+using namespace std;
+
+void swapArray(int* p, int* q, int size);
+void printArray(int* p,int size);
+
+int main() {
+	int a[] = { 1,2,3,4,5 };
+	int b[] = { 9,8,7,6,5 };
+	swapArray(a, b, 5);
+	cout << "a의 배열 ";
+	printArray(a,5);
+	cout << endl;
+	cout << "b의 배열 ";
+	printArray(b,5);
+
+	return 0;
+}
+
+void swapArray(int* p, int* q, int size) {
+	int i;
+	for (i = 0; i < size; i++) {
+		int change = p[i];
+		p[i] = q[i];
+		q[i] = change;
+	}
+}
+
+void printArray(int* p, int size) {
+	int i;
+	for (i = 0; i < size; i++) {
+		cout <<p[i]<<' ';
+	}
+}
+*/
