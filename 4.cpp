d7909123d70d284a218a6e9d1778919dea1f8cc9
2142920 <162396015+2142920@users.noreply.github.com>
2024-06-16 01:40:58 +0900

    Create 4.cpp

---
+++ 4.cpp
@@ -0,0 +1,35 @@
+/*
+#include <iostream>
+using namespace std;
+
+void reversed(int a[]);
+
+int main() {
+	int n[100]={0};
+
+	reversed(n);
+
+	for (int i = 99; i >= 0; --i) {
+		if (n[i] != 0) {
+			cout << n[i] << ' ';
+		}
+	}
+	cout << endl;
+	return 0;
+}
+
+void reversed(int a[]) {
+	int count = 0;
+	while (true) {
+		int n;
+		cout << "숫자를 입력하시오:";
+		cin >> n;
+		if (n == 0) {
+			break;
+		}
+		a[count] = n;
+		count = count + 1;
+
+	}
+}
+*/
