62b16ac77eea380a6f140b7e24f0cbd51a1c648d
2142920 <162396015+2142920@users.noreply.github.com>
2024-06-16 01:41:25 +0900

    Create 6.cpp

---
+++ 6.cpp
@@ -0,0 +1,29 @@
+/*
+#include <iostream>
+using namespace std;
+
+int factorial(int n);
+
+int main() {
+	int n;
+	cout << "숫자를 입력하시오.: ";
+	cin >> n;
+
+	cout << factorial(n);
+	return 0;
+}
+
+int factorial(int n) {
+	if (n == 0 || n == 1) {
+		return 1;
+	}
+	else {
+		int mul=1;
+		for (int i = 1; i <= n; i++) {
+			mul = mul * i;
+		}
+		return mul;
+	}
+	
+}
+*/
