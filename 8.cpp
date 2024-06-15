0781ddb93b987e0f93c820fb76e943892e4f897a
2142920 <162396015+2142920@users.noreply.github.com>
2024-06-16 01:41:48 +0900

    Create 8.cpp

---
+++ 8.cpp
@@ -0,0 +1,34 @@
+/*
+#include <iostream>
+using namespace std;
+
+int maximum(int a[], int size);
+int minimum(int a[], int size);
+
+int main(){
+	int array[10] = { 9,5,11,4,1,3,7,12,2,8 };
+
+	cout << "최대값: "<<maximum(array,10)<<endl;
+	cout << "최소값: " << minimum(array, 10);
+}
+
+int maximum(int a[], int size) {
+	int max = 0;
+	for (int i = 0; i < size; i++) {
+		if (a[i] > max) {
+			max = a[i];
+		}
+	}
+	return max;
+}
+
+int minimum(int a[], int size) {
+	int min = 100;
+	for (int i = 0; i < size; i++) {
+		if (a[i] < min) {
+			min = a[i];
+		}
+	}
+	return min;
+}
+*/
