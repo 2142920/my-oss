aae82d558cbd95cd4091474cae5756dfb207c930
2142920 <162396015+2142920@users.noreply.github.com>
2024-06-16 01:41:10 +0900

    Create 5.cpp

---
+++ 5.cpp
@@ -0,0 +1,21 @@
+/*
+#include <iostream>
+using namespace std;
+
+double area(int a);
+
+int main() {
+	int r;
+	cout << "반지름을 입력하시오:";
+	cin >> r;
+
+	cout << area(r);
+	return 0;
+}
+
+double area(int a) {
+	double area;
+	area = a * 3.14 * 3.14;
+	return area;
+}
+*/
